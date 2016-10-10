05c05c7573ec1cd11fa72fab15e36dfd6899cb30
aravind224 <v.aravind224@gmail.com>
2016-10-11 01:08:44 +0530

    Create numbers_switch.C

---
+++ numbers_switch.C
@@ -0,0 +1,35 @@
+#include "stdio.h"
+
+int main {
+
+int numb = '1';
+ 
+ switch(numb) {
+  case '1' :
+         printf("one\n" );
+         break;
+      case '2' :
+      case '3' :
+         printf("three\n" );
+         break;
+      case '4' :
+         printf("four\n" );
+         break;
+      case '5' :
+         printf("five\n" );
+         break;
+      case '6' :
+      	 printf("six\n");
+      	 break;
+      case '7' :
+         printf("seven\n");
+         break;
+      case '8' :
+      	printf("eigh\n");
+      	break;
+      case '9' :
+        printf("nine\n");
+        break;
+      default :
+         printf("number is\n" );
+   }
