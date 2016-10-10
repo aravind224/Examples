38822742c3c91cc48e970fa777434c12114ddb1e
aravind224 <v.aravind224@gmail.com>
2016-10-10 21:40:15 +0530

    Create vote.c

---
+++ vote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main () { 
+
+int age=18; 
+
+if ( age < 18 ){
+	printf("he cant vote \n");
+} else if (age==18) {
+	printf("congragulation \n");
+} else { 
+	printf("he can vote\n");
+}
+	return 0;
+}
