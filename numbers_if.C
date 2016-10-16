ecb20e57cc39e3fc5ddcfc11733ee0d5239dfebf
aravind224 <v.aravind224@gmail.com>
2016-10-16 21:13:56 +0530

    Update numbers_if.C

--- numbers_if.C
+++ numbers_if.C
@@ -1,40 +1,40 @@
 #include<stdio.h>
- 
+#include<conio.h>
 int main () {
-	
-int a=100;
- 
-if (a==1){
-	printf("one\n");
- }
-  if (a==2){
- 	printf("two\n");
- }
- if (a==3){
- 	printf("three\n");
- }	
-  if(a==4){
- 	printf("four\n");
- }
-  if (a==5){
-  	printf("five\n");
- }
-  if(a==6){
-	printf("six\n");
-  }
-  if(a==7){
-  	printf("seven\n");
-  }	
-  if(a==8){
-  	printf("eight\n");
-  }
-  if(a==9){
-  	printf("nine\n");
-  }
-  else
-  printf("sorry\n");
- 
-  
-return 0;
-
+	int a;
+	clrscr();
+	printf("enter the number\n");
+	scanf("%d",&a);
+	if (a==1){
+		printf("one\n");
+	}
+	else if (a==2){
+		printf("two\n");
+	}
+	else if (a==3){
+		printf("three\n");
+	}
+	else if(a==4){
+		printf("four\n");
+	}
+	else if (a==5){
+		printf("five\n");
+	}
+	else if(a==6){
+		printf("six\n");
+	}
+	else if(a==7){
+		printf("seven\n");
+	}
+	else if(a==8){
+		printf("eight\n");
+	}
+	else if(a==9){
+		printf("nine\n");
+	}
+	else {
+		printf("sorry\n");
+	}
+	getch();
+	return 0;
 }
