1b207b76556ae3cd8d7cda79993b357820880e61
aravind224 <v.aravind224@gmail.com>
2016-10-16 22:25:17 +0530

    Update numbers_switch.C

--- numbers_switch.C
+++ numbers_switch.C
@@ -1,35 +1,46 @@
-#include "stdio.h"
+#include <stdio.h>
+#include<conio.h>
 
-int main {
+void main() {
+	int numb;
+	int i;
+	i=8;
+	clrscr();
+	printf("enter the number");
+	scanf("%d",&numb);
+	//printf("%d",numb);
+	switch(numb) {
 
-int numb = '1';
- 
- switch(numb) {
-  case '1' :
-         printf("one\n" );
-         break;
-      case '2' :
-      case '3' :
-         printf("three\n" );
-         break;
-      case '4' :
-         printf("four\n" );
-         break;
-      case '5' :
-         printf("five\n" );
-         break;
-      case '6' :
-      	 printf("six\n");
-      	 break;
-      case '7' :
-         printf("seven\n");
-         break;
-      case '8' :
-      	printf("eigh\n");
-      	break;
-      case '9' :
-        printf("nine\n");
-        break;
-      default :
-         printf("number is\n" );
-   }
+		case 1 :
+			printf("one\n" );
+		break;
+		case 2 :
+			printf("two\n");
+		break;
+		case 3 :
+			printf("three\n" );
+		break;
+		case 4 :
+			printf("four\n" );
+		break;
+		case 5 :
+			printf("five\n" );
+		break;
+		case 6 :
+			printf("six\n");
+		break;
+		case 7 :
+			printf("seven\n");
+		break;
+		case 8 :
+			printf("eight\n");
+		break;
+		case 9 :
+			printf("nine\n");
+		break;
+		default :
+			printf("the number is %d",numb);
+	}
+	getch();
+
+}
